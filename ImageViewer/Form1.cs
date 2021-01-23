981ed2cd4f095a2a227e71ad2b3808dad5a38f55
Mr-Ojii <okaschan@gmail.com>
2021-01-24 08:32:47 +0900

    ファイルの追加

---
+++ ImageViewer/Form1.cs
@@ -0,0 +1,63 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Drawing;
+using System.Text;
+using System.IO;
+using System.Windows.Forms;
+
+namespace ImageViewer
+{
+    public partial class Form1 : Form
+    {
+        string FilePath;
+        public Form1(string FilePath)
+        {
+            InitializeComponent();
+            this.FilePath = FilePath;
+            LoadImage();
+        }
+        protected override void OnLoad(EventArgs e)
+        {
+            base.OnLoad(e);
+        }
+        protected override void OnKeyDown(KeyEventArgs e)
+        {
+            if (e.KeyCode == Keys.Left)
+            {
+                Change(-1);
+            }
+            else if (e.KeyCode == Keys.Right)
+            {
+                Change(1);
+            }
+            base.OnKeyDown(e);
+        }
+
+        private void Change(int c) 
+        {
+            string[] files = Directory.GetFiles(Path.GetDirectoryName(FilePath));
+            for (int i = (Array.IndexOf(files, FilePath) + c + files.Length) % files.Length; ; i = (i + c + files.Length) % files.Length) 
+            {
+                try
+                {
+                    FilePath = files[i];
+                    LoadImage();
+                    break;
+                }
+                catch (Exception ex)
+                {
+                    Console.WriteLine(ex.ToString());
+                    continue;
+                }
+            }
+        }
+
+        private void LoadImage()
+        {
+            this.BackgroundImage?.Dispose();
+            this.BackgroundImage = Image.FromFile(FilePath);
+            this.BackgroundImageLayout = ImageLayout.Zoom;
+        }
+    }
+}
---
+++ ImageViewer/Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Windows.Forms;
+
+namespace ImageViewer
+{
+    static class Program
+    {
+        /// <summary>
+        /// アプリケーションのメイン エントリ ポイントです。
+        /// </summary>
+        [STAThread]
+        static void Main(string[] args)
+        {
+            Application.EnableVisualStyles();
+            Application.SetCompatibleTextRenderingDefault(false);
+            if (args.Length != 0)
+            {
+                if (string.IsNullOrEmpty(args[0]) || !File.Exists(args[0]))
+                {
+                    MessageBox.Show("有効なパスではありません。", "ERROR", MessageBoxButtons.OK, MessageBoxIcon.Error);
+                    return;
+                }
+                Application.Run(new Form1(args[0]));
+            }
+            else 
+            {
+                MessageBox.Show("引数でファイルを指定してください。", "ERROR", MessageBoxButtons.OK, MessageBoxIcon.Error);
+                return;
+            }
+        }
+    }
+}
